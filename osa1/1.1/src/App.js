77f4f8c0bd92928a4fa0d1058385a710fae84e88
Tuuli <silvennoinen.tuuli@gmail.com>
2022-07-05 15:34:06 +0300

    1.1: kurssitiedot, step1

---
+++ osa1/1.1/src/App.js
@@ -0,0 +1,46 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.content} {props.amount}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  )
+
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content content={part1} amount={exercises1} />
+      <Content content={part2} amount={exercises2} />
+      <Content content={part3} amount={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App

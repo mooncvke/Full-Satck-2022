e5127b8f056f967d3601750d50c19c4812622c6f
Tuuli <silvennoinen.tuuli@gmail.com>
2022-07-18 17:52:58 +0300

    1.10: unicafe step5

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,21 +1,52 @@
 import { useState } from 'react'
 
+const StatisticLine = (props) => {
+  return (
+    <div>
+    <p>{props.text} {props.value}</p>
+  </div>
+  )
+}
+
+const All = (props) => {
+  return (
+    props.allClicks.length
+  )
+}
+
+const Avg = (props) => {
+  const avg = (props.good * 1 + props.bad * -1) / props.allClicks.length
+  return (
+    avg
+  )
+}
+
+const Pos = (props) => {
+  const percent = props.good / props.allClicks.length * 100
+  return (
+    percent
+  )
+
+}
+
 const Statistics = (props) => {
   if (props.allClicks.length == 0) {
     return (
       <p>No feedback given</p>
     )
   } else {
-    const avg = (props.good * 1 + props.bad * -1) / props.allClicks.length
-    const percent = props.good / props.allClicks.length * 100
-      return (
+    const all = <All allClicks={props.allClicks} good={props.good} neutral={props.neutral} bad={props.bad} />
+    const avg = <Avg allClicks={props.allClicks} good={props.good} neutral={props.neutral} bad={props.bad} />
+    const pos = <Pos allClicks={props.allClicks} good={props.good} neutral={props.neutral} bad={props.bad} />
+
+    return (
         <div>
-          <p>good {props.good}</p>
-          <p>neutral {props.neutral}</p>
-          <p>bad {props.bad}</p>     
-          <p>{props.text} {props.allClicks.length}</p>
-          <p>average {avg}</p>
-          <p>positive {percent} %</p>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={avg} />
+          <StatisticLine text="positive" value={pos } />
         </div> 
       )
     }
@@ -58,11 +89,11 @@ const App = () => {
       </div>
       <div>
         <h1>statistics</h1>
-        <Statistics text="all" allClicks={allClicks} good={good} neutral={neutral} bad={bad} />
+        <Statistics allClicks={allClicks} good={good} neutral={neutral} bad={bad} />
       </div>
 
     </div>
   )
+  
 }
-
 export default App;

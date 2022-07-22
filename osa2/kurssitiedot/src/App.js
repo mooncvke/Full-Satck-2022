2ca1d4e9694a78f42669e751c16128fb8c40d5da
Tuuli <silvennoinen.tuuli@gmail.com>
2022-07-22 13:17:31 +0300

    2.4

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,7 +1,7 @@
 const Header = ({course}) => {
   return (
     <div>
-      <h1>{course.name}</h1>
+      <h2>{course.name}</h2>
     </div>
   )
 }
@@ -21,7 +21,7 @@ const Total = ({course}) => {
 
   return (
     <div>
-      <p>Number of exercises {total}</p>
+      <h4>total of {total} exercises</h4>
     </div>
   )
 
@@ -47,36 +47,57 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Web developement curriculum</h1>
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      )) }
     </div>
   )
 }

30782a8f645ce9bd691a1cd1ad51f4976778ea3d
Tuuli <silvennoinen.tuuli@gmail.com>
2022-07-29 11:05:38 +0300

    2.15

--- osa2/phonebook/src/App.js
+++ osa2/phonebook/src/App.js
@@ -1,67 +1,80 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import Persons from './components/Persons'
-import PersonForm from './components/PersonForm'
-import Filter from './components/Filter'
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import Filter from "./components/Filter";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   useEffect(() => {
-    console.log('effect')
-    axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-        setPersons(response.data)
-    })
-  }, [])
-  console.log('render', persons.length, 'persons')
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const addName = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const nameObject = {
-    name: newName,
-    number: newNumber,
-    id: persons.length + 1 }
-    
-    { persons.map(person => 
-      { person.name === newName ? alert(`${newName} is already added to phonebook`) :  
-      setPersons(persons.concat(nameObject))
-      setNewName('') 
-      setNewNumber('')
-      })}
-  }
-  const handleNameChange = ( event ) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
 
-  const handleNumberChange = ( event ) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
+    {
+      persons.map((person) => {
+        person.name === newName
+          ? alert(`${newName} is already added to phonebook`)
+          : axios
+              .post("http://localhost:3001/persons", nameObject)
+              .then((response) => {
+                console.log(response);
+                setPersons(persons.concat(nameObject));
+                setNewName("");
+                setNewNumber("");
+              });
+      });
+    }
+  };
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
 
-  const handleFilterChange = ( event ) => {
-    console.log(event.target.value)
-    setNewFilter(event.target.value)
-  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setNewFilter(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
-      
+
       <h2>Add a new</h2>
-      <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
+      <PersonForm
+        addName={addName}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
 
       <h2>Numbers</h2>
-      <Persons persons={persons} newFilter={newFilter}/>
+      <Persons persons={persons} newFilter={newFilter} />
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;

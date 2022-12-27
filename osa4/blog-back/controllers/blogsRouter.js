1edf34a5d8382c0aea18a73233ef0a7be6bdcc8b
Tuuli Silvennoinen <tuuli.silvennoinen@tuni.fi>
2022-12-27 15:19:49 +0200

    delete working

--- osa4/blog-back/controllers/blogsRouter.js
+++ osa4/blog-back/controllers/blogsRouter.js
@@ -6,36 +6,40 @@ const logger = require("../utils/logger");
 const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
-  response.json(blogs);
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
+  response.json(blogs.map((blog) => blog.toJSON()));
 });
 
 blogsRouter.get("/:id", async (request, response) => {
   logger.info(request);
   const blog = await Blog.findById(request.params.id);
-  response.json(blog.toJSON());
+  if (blog === null) {
+    response.json("no content");
+  } else {
+    response.json(blog.toJSON());
+  }
 });
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!request.token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
+  if (!body.likes) {
+    body.likes = 0;
   }
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
 
   const user = await User.findById(decodedToken.id);
 
-  var likes = 0;
-  if (body.likes != undefined) {
-    likes = body.likes;
-  }
-
   const blog = new Blog({
-    user: user._id,
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: likes,
+    likes: body.likes,
+    user: user._id,
   });
 
   const savedBlog = await blog.save();
@@ -44,35 +48,13 @@ blogsRouter.post("/", async (request, response) => {
   response.status(201).json(savedBlog.toJSON);
 });
 
-blogsRouter.put("/:id", async (request, response) => {
-  const body = request.body;
-
-  const blog = {
-    author: body.author,
-    title: body.title,
-    url: body.url,
-    likes: body.likes,
-  };
-  await Blog.findById(request.params.id);
-
-  await Blog.findByIdAndUpdate(request.params.id, blog);
-  const savedBlog = await Blog.findById(request.params.id);
-  console.log("blog: ", Blog);
-  console.log("savedBlog: ", savedBlog);
-  console.log("requestparansid: ", request.params.id);
-  response.json(savedBlog.toJSON());
-});
-
 blogsRouter.delete("/:id", async (request, response) => {
   const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!request.token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
-  }
-  const user = await User.findById(decodedToken.id);
+  console.log("request: ", request.params.id); // etsi kuka on alunperin postannu tän blogin
+  console.log("decodedtoken: (tuulipuulin id) ", decodedToken.id);
+  const savedBlog = await Blog.findById(request.params.id);
 
-  await Blog.findById(request.params.id);
-  console.log("bloguserb", blog.user);
-  if (user.id !== decodedToken.id) {
+  if (savedBlog.user.toString() !== decodedToken.id.toString()) {
     return response
       .status(401)
       .json({ error: "Cant't delete blogs if not signed in" });
--- osa4/blog-back/utils/middleware.js
+++ osa4/blog-back/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require("./logger");
+const Blog = require("../models/blog");
+
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
   logger.info("Path:  ", request.path);
@@ -12,30 +14,38 @@ const tokenExtractor = (request, response, next) => {
   if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
     request.token = authorization.substring(7);
   }
+  next();
+};
 
+const userExtractor = (request, response, next) => {
+  const savedBlog = Blog.findById(request.params._id);
+  console.log("saved blog: ", savedBlog.user);
+  request.user = savedBlog.user;
   next();
 };
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
-  if (error.name === "CastError") {
+  if (error.name === "CastError" && error.kind === "ObjectId") {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
   } else if (error.name === "JsonWebTokenError") {
     return response.status(401).json({
-      error: "invalid token",
+      error: "token missing or invalid token",
     });
   }
-  logger.error(error.message);
   next(error);
 };
+
 module.exports = {
   requestLogger,
+  userExtractor,
   tokenExtractor,
   unknownEndpoint,
   errorHandler,
